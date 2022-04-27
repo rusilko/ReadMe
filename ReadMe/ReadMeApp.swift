62a5a022ebfc7af5526d84840834a43c6937aa4e
rusilko <tomek.rusilko@lunarlogic.io>
2022-04-27 21:40:22 +0200

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by tomek on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
